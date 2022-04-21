ec881c8804f86dc9ea871cd19c8cbebd35b5b1ef
ManjUNooB <ryuzin20000901@gmail.com>
2022-04-22 08:56:32 +0900

    新プロジェクトの追加。まだStartUpの途中書いてた気がする

---
+++ DirectX11_from_the_begging/DirectX11_from_the_begging/Startup.cpp
@@ -0,0 +1,18 @@
+//----------------------------------------------------//
+//	Contents	:	Startup.cpp
+//					初期化だったりWINAPI関連だったり
+//					FPS管理だったり
+//	Author		:	Ryuga Sakakibara
+//	LastUpdate	:	2022/04/20
+//	Since		:	2022/04/20
+//----------------------------------------------------//
+#include <Windows.h>
+#include <cstdio>
+#include <crtdbg.h>
+#include "Defines.h"
+#include "App/main.h"
+
+//	timeGetTime周りの使用
+#pragma comment(lib,"winmm.lib")
+
+//--プロトタイプ宣言--//
